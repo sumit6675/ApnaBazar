fec8eac14e34bf117a44dac4d610d1a5c7348f58
sumit6675 <sumitchimkar0@gmail.com>
2023-02-07 13:55:45 +0530

    adding backend data for filter and single product page

--- Backend/routes/Product.route.js
+++ Backend/routes/Product.route.js
@@ -60,6 +60,16 @@ ProductRoute.get("/phone", async (req, res) => {
   } catch (e) {}
 });
 
+ProductRoute.get("/phone/:id",async(req,res)=>{
+  let id=req.params.id
+  try{
+      let data= await ProductModule.findById(id)
+      res.send(data)
+  }catch(e){
+
+  }
+})
+
 ProductRoute.get("/laptop", async (req, res) => {
   const { page = 1, limit = 16 } = req.query;
   const { brand } = req.query;
@@ -112,27 +122,145 @@ ProductRoute.get("/laptop", async (req, res) => {
     }
   } catch (e) {}
 });
+
+ProductRoute.get("/laptop/:id",async(req,res)=>{
+  let id=req.params.id
+  try{
+      let data= await ProductModule.findById(id)
+      res.send(data)
+  }catch(e){
+
+  }
+})
+
 ProductRoute.get("/women", async (req, res) => {
   const { page = 1, limit = 16 } = req.query;
-  let data = await ProductModule.find({ category: "Women Clothing" })
-    .limit(limit * 1)
-    .skip((page - 1) * limit);
-  res.send(data);
+  const { brand } = req.query;
+  try {
+    if (brand === "saree") {
+      let data = await ProductModule.find({
+        Name: { $regex: /.*saree.*/i },
+        category: "Women Clothing",
+      })
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      res.send(data);
+    } else if (brand === "Dress") {
+      let data = await ProductModule.find({
+        Name: { $regex: /.*Dress.*/i },
+        category: "Women Clothing",
+      })
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      res.send(data);
+    } else if (brand === "All") {
+      let data = await ProductModule.find({ category: "Women Clothing" })
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      res.send(data);
+    }
+  } catch (e) {}
 });
+ProductRoute.get("/women/:id",async(req,res)=>{
+  let id=req.params.id
+  try{
+      let data= await ProductModule.findById(id)
+      res.send(data)
+  }catch(e){
+
+  }
+})
 ProductRoute.get("/men", async (req, res) => {
   const { page = 1, limit = 16 } = req.query;
-  let data = await ProductModule.find({ category: "Men Clothing" })
-    .limit(limit * 1)
-    .skip((page - 1) * limit);
-  res.send(data);
+  const { brand } = req.query;
+  try {
+    if (brand === "Jacket") {
+      let data = await ProductModule.find({
+        Name: { $regex: /.*Jacket.*/i },
+        category: "Men Clothing",
+      })
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      res.send(data);
+    } else if (brand === "Puma") {
+      let data = await ProductModule.find({
+        Name: { $regex: /.*Puma.*/i },
+        category: "Men Clothing",
+      })
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      res.send(data);
+    } else if (brand === "High Neck Jacket") {
+      let data = await ProductModule.find({
+        Name: { $regex: /.*High Neck Jacket.*/i },
+        category: "Men Clothing",
+      })
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      res.send(data);
+    } else if (brand === "All") {
+      let data = await ProductModule.find({ category: "Men Clothing" })
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      res.send(data);
+    }
+  } catch (e) {}
 });
-ProductRoute.get("/home", async (req, res) => {
+ProductRoute.get("/men/:id",async(req,res)=>{
+  let id=req.params.id
+  try{
+      let data= await ProductModule.findById(id)
+      res.send(data)
+  }catch(e){
+
+  }
+})
+
+ProductRoute.get("/home", async (req, res) =>  {
   const { page = 1, limit = 16 } = req.query;
-  let data = await ProductModule.find({ category: "Home Appliances" })
-    .limit(limit * 1)
-    .skip((page - 1) * limit);
-  res.send(data);
+  const { brand } = req.query;
+  try {
+    if (brand === "Masala") {
+      let data = await ProductModule.find({
+        Name: { $regex: /.*Masala.*/i },
+        category: "Home Appliances",
+      })
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      res.send(data);
+    } else if (brand === "Roll") {
+      let data = await ProductModule.find({
+        Name: { $regex: /.*Roll.*/i },
+        category: "Home Appliances",
+      })
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      res.send(data);
+    } else if (brand === "Ready To Eat") {
+      let data = await ProductModule.find({
+        Name: { $regex: /.*285g.*/i },
+        category: "Home Appliances",
+      })
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      res.send(data);
+    } else if (brand === "All") {
+      let data = await ProductModule.find({ category: "Home Appliances" })
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      res.send(data);
+    }
+  } catch (e) {}
 });
+ProductRoute.get("/home/:id",async(req,res)=>{
+  let id=req.params.id
+  try{
+      let data= await ProductModule.findById(id)
+      res.send(data)
+  }catch(e){
+
+  }
+})
 
 // ProductRoute.patch("/update",async(req,res)=>{
 //    let data=await ProductModule.find()
