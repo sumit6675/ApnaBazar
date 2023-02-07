ab80a98d3e80d3bcaffdfd75a0cd3161ee050b66
sumit6675 <sumitchimkar0@gmail.com>
2023-02-08 02:28:12 +0530

    adding frontend for single product page and also adding sorting funcationality for discount,price,rating etc

--- Frontend/src/Pages/SingleProductPage.jsx
+++ Frontend/src/Pages/SingleProductPage.jsx
@@ -2,22 +2,24 @@ import { StarIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
-  Flex,
+  Container,
   Grid,
-  GridItem,
   Heading,
   Image,
   Input,
   ListItem,
   Text,
   UnorderedList,
+  VStack,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ProductCarousal from "../Componets/Homepage/ProductCarousal";
 
 const SingleProduct = ({ category }) => {
   const [singleData, setSingleData] = useState([]);
-  const [prodductData,setProductData]=useState([])
+  const [prodductData, setProductData] = useState([]);
+  const [menData,setMenData]=useState([])
   const params = useParams();
 
   useEffect(() => {
@@ -26,66 +28,31 @@ const SingleProduct = ({ category }) => {
       .then((res) => res.json())
       .then((res) => {
         setSingleData(res);
-        setProductData(res.selection6)
+        setProductData(res.selection6);
+        let men=singleData.selection2
+        setMenData(men.split("\n"))
       });
-  }, [category, params]);
-console.log(prodductData)
+  }, [category, params,singleData.selection2]);
+  console.log(prodductData);
   return (
-    <>
-      <Box marginTop={12}>
-        <Grid
-          h={["1300px", "100px", "600px"]}
-          templateRows={["repeat(8, 1fr)", "repeat(8, 1fr)", "repeat(8, 1fr)"]}
-          templateColumns={[
-            "repeat(4, 1fr)",
-            "repeat(6, 1fr)",
-            "repeat(10, 1fr)",
-          ]}
-        >
-          <GridItem
-            rowSpan={[1, 2, 4]}
-            colSpan={[4, 6, 4]}
-            display="flex"
-            justifyContent="center"
-            alignitem="center"
-            style={{
-              boxShadow:
-                "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px",
-            }}
-          >
-            <Image
-              src={singleData.image}
-              alt={singleData.Name}
-              _hover={{ cursor: "crosshair" }}
-            />
-          </GridItem>
-
-          <GridItem
-            colSpan={[4, 6, 6]}
-            rowSpan={1}
-            p={5}
-            style={{
-              boxShadow:
-                "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px",
-            }}
-          >
-            <Text color="gray.500" marginBottom={5}>
-              Article ID: {singleData._id}
-            </Text>
-            <Heading size="md" marginBottom={5}>
+    <Box p="5">
+      <Grid gridTemplateColumns={"0.25fr 0.40fr 0.35fr"}>
+        <Container>
+          <Image
+            src={singleData.image}
+            _hover={{ cursor: "crosshair" }}
+            alt="image"
+            w="85%"
+          />
+        </Container>
+        <Grid>
+          <Container>
+            <Heading marginBottom={3} size="md">
               {singleData.Name}
             </Heading>
-          </GridItem>
-
-          <GridItem
-            colSpan={[4, 3, 3]}
-            rowSpan={7}
-            style={{
-              boxShadow:
-                "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px",
-            }}
-            p={5}
-          >
+            <Text marginBottom={3} size="md">
+              ProductId: {singleData._id}
+            </Text>
             <Heading size="sm" marginBottom={3}>
               Gain more with offers (4)
             </Heading>
@@ -139,6 +106,19 @@ console.log(prodductData)
               </ListItem>
             </UnorderedList>
             <Heading size="sm" marginBottom={3}>
+              Product Details
+            </Heading>
+            <UnorderedList color="gray.600" fontSize="sm" marginBottom={4}>
+              {category!=="men" && prodductData.map((i) => (
+                <ListItem key="i._id">{i.name}</ListItem>
+              ))}
+            </UnorderedList>
+            <UnorderedList color="gray.600" fontSize="sm" marginBottom={4}>
+              {category==="men" && menData.map((i) => (
+                <ListItem key={i}>{i}</ListItem>
+              ))}
+            </UnorderedList>
+            <Heading size="sm" marginBottom={3}>
               Additional Services & Warranties (3){" "}
               <span
                 style={{
@@ -150,98 +130,92 @@ console.log(prodductData)
                 View All
               </span>
             </Heading>
-          </GridItem>
-
-          <GridItem
-            colSpan={[4, 3, 3]}
-            rowSpan={7}
-            p={5}
-            style={{
-              boxShadow:
-                "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px",
-            }}
-          >
-            <Heading size="lg" marginBottom={5} color="blue.700">
-              ₹{singleData.price}
-            </Heading>
-            <Box display="flex" mt="2" alignItems="center">
-              {Array(5)
-                .fill("")
-                .map((_, i) => (
-                  <StarIcon
-                    key={i}
-                    color={i < singleData.rating ? "teal.500" : "gray.300"}
-                  />
-                ))}
-              <Box as="span" ml="2" color="gray.600" fontSize="sm">
-                {singleData.reviewNumber} reviews
-              </Box>
-            </Box>
-            <Text fontSize="lg" marginBottom={3}>
-              MRP:{" "}
-              <span style={{ textDecoration: "line-through" }}>
-                {singleData.mrp}
-              </span>{" "}
-              <span style={{ fontSize: "12px" }}>(Inclusive of all taxes)</span>
-            </Text>
-
-            <Text
-              fontSize="sm"
-              color="green.600"
-              style={{ fontWeight: "bold" }}
-              marginBottom={3}
-            >
-              You Save: {singleData.discount}%
-            </Text>
-
-            <Text fontSize="sm" style={{ fontWeight: "bold" }} marginBottom={3}>
-              EMIs (Credit Cards) from ₹792.16/month |{" "}
-              <span style={{ color: "#2871c4" }}>View Plans</span>
-            </Text>
-
-            <Text fontSize="lg" style={{ fontWeight: "bold" }} marginBottom={3}>
-              FREE Shipping!
-            </Text>
-
-            <Input
-              w="70%"
-              borderRadius="none"
-              placeholder="Enter / Detect PIN Code"
-              p={2}
-              marginBottom={3}
-            ></Input>
-
-            <Flex w="full" justifyContent="space-between">
-              <Button
-                w="49%"
-                color="white"
-                bg="red"
-                borderRadius="sm"
-                fontSize="lg"
-                p={6}
-                _hover={{ bg: "blue.800" }}
-                //   onClick={() => handlePost(singleData)}
-              >
-                ADD TO CART
-              </Button>
-              <Button
-                w="49%"
-                color="white"
-                bg="orangered"
-                borderRadius="sm"
-                fontSize="lg"
-                p={6}
-                _hover={{ backgroundColor: "orangered" }}
-                //   onClick={() => handleWish(singleData)}
-              >
-                Add to Wishlist
-              </Button>
-            </Flex>
-          </GridItem>
-
+          </Container>
         </Grid>
-      </Box>
-    </>
+        <VStack>
+          <Heading size="lg" marginBottom={5} color="blue.700">
+            ₹{singleData.price}
+          </Heading>
+          <Box display="flex" mt="2" alignItems="center">
+            {Array(5)
+              .fill("")
+              .map((_, i) => (
+                <StarIcon
+                  key={i}
+                  color={i < singleData.rating ? "teal.500" : "gray.300"}
+                />
+              ))}
+            <Box as="span" ml="2" color="gray.600" fontSize="md">
+              {singleData.reviewNumber} reviews
+            </Box>
+          </Box>
+          <Text fontSize="lg" marginBottom={1}>
+            MRP:{" "}
+            <span style={{ textDecoration: "line-through" }}>
+              {singleData.mrp}
+            </span>{" "}
+            <span style={{ fontSize: "12px" }}>(Inclusive of all taxes)</span>
+          </Text>
+
+          <Text
+            fontSize="md"
+            color="green.600"
+            style={{ fontWeight: "bold" }}
+            marginBottom={3}
+          >
+            You Save: {singleData.discount}%
+          </Text>
+
+          <Text fontSize="md" style={{ fontWeight: "bold" }} marginBottom={3}>
+            EMIs (Credit Cards) from ₹792.16/month |{" "}
+            <span style={{ color: "#2871c4" }}>View Plans</span>
+          </Text>
+
+          <Text fontSize="lg" style={{ fontWeight: "bold" }} marginBottom={3}>
+            FREE Shipping!
+          </Text>
+
+          <Input
+            w="70%"
+            borderRadius="none"
+            placeholder="Enter / Detect PIN Code"
+            p={2}
+            marginBottom={3}
+          ></Input>
+
+          <VStack w="full" justifyContent="space-between">
+            <Button
+              w="45%"
+              color="white"
+              bg="red"
+              borderRadius="sm"
+              fontSize="lg"
+              p={6}
+              _hover={{ bg: "blue.800" }}
+              //   onClick={() => handlePost(singleData)}
+            >
+              ADD TO CART
+            </Button>
+            <Button
+              w="45%"
+              color="white"
+              bg="orangered"
+              borderRadius="sm"
+              fontSize="lg"
+              p={6}
+              _hover={{ backgroundColor: "orangered" }}
+              //   onClick={() => handleWish(singleData)}
+            >
+              Add to Wishlist
+            </Button>
+          </VStack>
+        </VStack>
+      </Grid>
+      <Heading margin={"auto"} mt="3" marginBottom={3} size="md">
+        Related products with free delivery on eligible orders
+      </Heading>
+      <ProductCarousal phone={category} count={5} />
+    </Box>
   );
 };
 
